ab1883edbc5670a89e3abd49d886ed8c5fcdddfa
Seb <seb@seb.fr>
2018-05-04 20:11:09 +0200

    init

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import * as express from 'express';
 import * as fs from 'fs';
+import * as _ from "lodash";
 
 const app: express.Application = express();
 
 let tunes = fs.readdirSync('./tunes');
+let randomTunePath = '/home/pi/random-tune/tunes/' + tunes[Math.floor(Math.random() * tunes.length)];
 
 app.get('/random-tune', (req, res) => {
-    //let randomTunePath = '/home/pi/random-tune/tunes/' + tunes[Math.floor(Math.random() * tunes.length)];
-    let randomTunePath = '/home/pi/random-tune/tunes/' + tunes[0];
-    console.log('req:', req);
     console.log('Serving ', randomTunePath);
+    _.debounce(() => {
+        randomTunePath = '/home/pi/random-tune/tunes/' + tunes[Math.floor(Math.random() * tunes.length)];
+    }, 500);
     res.sendFile(randomTunePath);
 });
 
